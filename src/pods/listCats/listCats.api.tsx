c25e93d41f3bff40f82cf3a659553976f0c3eb36
Oscar <oscararnu18@gmail.com>
2024-11-28 16:15:28 +0100

    filter for cats added

--- src/pods/listCats/listCats.api.tsx
+++ src/pods/listCats/listCats.api.tsx
@@ -5,21 +5,26 @@ export const fetchDataListCats = async (page: number, setLoadingCats: { ({ }: bo
     const apiKey = import.meta.env.VITE_CATS_API_KEY;
     setLoadingCats(true);
     try {
-        const response = await fetch(`https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=DESC&page=${page}&limit=100&api_key=${apiKey}`);
+        const response = await fetch(`https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=DESC&page=${page}&limit=200&api_key=${apiKey}`);
         const data = await response.json();
 
         if (response.ok) {
 
-            const cats = data.map((cat: CatsData) => ({
-                id: cat.id,
-                picUrl: cat.url,
-                title: cat.breeds[0]?.name,
-                temperament: cat.breeds[0]?.temperament,
-                description: cat.breeds[0]?.description,
-                selected: false
-            }
+            const cats = data
+                .filter((cat: CatsData) =>
+                    cat.breeds[0]?.name.length > 0 &&
+                    cat.breeds[0]?.name !== 'Aegean' &&
+                    cat.breeds[0]?.name !== 'Abyssinian'
+                )
+                .map((cat: CatsData) => ({
+                    id: cat.id,
+                    picUrl: cat.url,
+                    title: cat.breeds[0]?.name,
+                    temperament: cat.breeds[0]?.temperament,
+                    description: cat.breeds[0]?.description,
+                    selected: false
+                }));
 
-            ));
             console.log(cats);
             return cats;
         }
@@ -31,7 +36,3 @@ export const fetchDataListCats = async (page: number, setLoadingCats: { ({ }: bo
         setLoadingCats(false);
     }
 }
-
-// const filterList = (cats: CatsData[]) => {
-//     cats.filter()
-// }
